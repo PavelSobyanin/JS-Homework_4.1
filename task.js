b76daec89d356dde0abb305c4e8a86bd3a320a95
Pavel Sobyanin <pavel_sobyanin_git@mail.ru>
2023-12-19 20:39:58 +0300

    Initial commit

---
+++ task.js
@@ -0,0 +1,26 @@
+let listItems = document.querySelectorAll(".reveal");
+
+const isInViewport = function (element) {
+	const viewportHeight = window.innerHeight;
+	const elementTop = element.getBoundingClientRect().top;
+	const elementHeight = element.getBoundingClientRect().height;
+	return elementTop < viewportHeight && elementTop > -elementHeight;
+};
+
+function checkReveals() {
+  [...listItems].forEach(element => {
+		if (isInViewport(element)) {
+			if (!element.classList.contains("reveal_active")) {
+				element.classList.add("reveal_active");
+			}
+		} else {
+			if (element.classList.contains("reveal_active")) {
+				element.classList.remove("reveal_active");
+			}
+		}
+	});
+}
+
+window.addEventListener("scroll", checkReveals, false);
+
+checkReveals();
